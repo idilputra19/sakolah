e204ba5d1fad680fee54b28a665271960d6dbe12
Triadmoko Denny Fatrosa <triadmoko.fatrosa@codemi.co.id>
2023-07-16 20:00:29 +0700

    first commit

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,143 @@
+<section>
+	<div class="flex flex-col md:flex-row w-full">
+		<div class="bg-primary md:w-1/2 md:h-screen p-6 flex flex-col justify-center items-center">
+			<h1 class="font-bold text-xl sm:text-2xl lg:text-3xl text-center">
+				Welcome to our largest community
+			</h1>
+			<h2 class="text-center font-medium">Let's learn something new today!</h2>
+			<img src="./assets/img/bg/02.svg" alt="" />
+			<div class="my-6 flex flex-col md:flex-row">
+				<div class="flex -space-x-1">
+					<img
+						class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+						src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt=""
+					/>
+					<img
+						class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+						src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt=""
+					/>
+					<img
+						class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+						src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+						alt=""
+					/>
+					<img
+						class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt=""
+					/>
+				</div>
+				<p class="md:ml-10 lg:ml-5 my-auto">4k+ Students joined us, now it's your turn.</p>
+			</div>
+		</div>
+		<div class="md:w-1/2 md:h-screen p-6 flex flex-col justify-center items-center">
+			<div>
+				<h1 class="font-bold text-xl sm:text-2xl lg:text-3xl text-left">👋</h1>
+				<h1 class="font-bold text-xl sm:text-2xl lg:text-3xl text-left">Login into Eduport!</h1>
+				<h2 class="text-center text-slate-500 font-medium">
+					Nice to see you! Please log in with your account.
+				</h2>
+			</div>
+			<div class="my-6 flex flex-col md:flex-row">
+				<form method="post" action="">
+					<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5 text-gray-400"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<input
+							class="outline-none border-none w-1/2"
+							type="text"
+							name=""
+							id=""
+							placeholder="Full name"
+						/>
+					</div>
+					<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5 text-gray-400"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+							/>
+						</svg>
+						<input
+							class="pl-2 outline-none border-none"
+							type="text"
+							name=""
+							id=""
+							placeholder="Username"
+						/>
+					</div>
+					<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5 text-gray-400"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+							/>
+						</svg>
+						<input
+							class="pl-2 outline-none border-none"
+							type="text"
+							name=""
+							id=""
+							placeholder="Email Address"
+						/>
+					</div>
+					<div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5 text-gray-400"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<input
+							class="pl-2 outline-none border-none"
+							type="text"
+							name=""
+							id=""
+							placeholder="Password"
+						/>
+					</div>
+					<button
+						type="submit"
+						class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+						>Login</button
+					>
+					<span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
